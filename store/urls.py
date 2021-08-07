a9c314492de15ec4c40abb9d2406456ae6eff9e4
Mohammd Alavi <yourhonor1996@gmail.com>
2021-08-07 19:17:48 +0430

    Change to class based views

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.store, name= 'store'),
-    path('cart/', views.cart, name= 'cart'),
-    path('checkout/', views.checkout, name= 'checkout')
+    path('', views.StoreView.as_view(), name= 'store'),
+    path('cart/', views.CartView.as_view(), name= 'cart'),
+    path('checkout/', views.CheckoutView.as_view(), name= 'checkout')
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -2,28 +2,32 @@ from store.models import Customer, Product, Order
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
 
+class CartView(View):
+    
+    def get(self, request):
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer= customer, complete= False)
+            items = order.orderitem_set.all()
+        else:
+            items = []
+            order = {'get_cart_total':0, 'get_cart_items_count':0}
+        
+        context = {'items':items, 'order': order}
+        return render(request, 'store/cart.html', context)
+    
+class StoreView(View):
+    
+    def get(self, request):
+        products = Product.objects.all()
+    
+        context = {'products':products}
+        return render(request, 'store/store.html', context)
 
-def store(request):
-    products = Product.objects.all()
+class CheckoutView(View):
     
-    context = {'products':products}
-    return render(request, 'store/store.html', context)
-    
-def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer= customer, complete= False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items_count':0}
-    
-    context = {'items':items, 'order': order}
-    return render(request, 'store/cart.html', context)
-    
-def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
- 
+    def get(self, request):
+        context = {}
+        return render(request, 'store/checkout.html', context)
+
